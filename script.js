7fdc871570b7b6eb34fe1578eab8696b75c3c879
unknown <ijlnflhbrzdev@gmail.com>
2021-02-26 22:19:15 +0700

    debugging style grid & disable function link

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function OpenSlide() {
+function OpenSlides() {
      document.querySelector('.DashboardNav').classList.toggle("OpenSlide");
 }
 
-const NoneParentMenu = document.querySelectorAll(".navLink");
-console.log(NoneParentMenu)
+// const NoneParentMenu = document.querySelectorAll(".navLink");
+// console.log(NoneParentMenu)
 
 
-if (document.body.clientWidth <= 992) {
-     NoneParentMenu.forEach(function (eTargets) {
-          eTargets.addEventListener('click', function (e) {
-            e.target.parentElement.parentElement.parentElement.parentElement.classList.toggle("OpenSlide")
-          })
-     })
-}
+// if (document.body.clientWidth <= 992) {
+//      NoneParentMenu.forEach(function (eTargets) {
+//           eTargets.addEventListener('click', function (e) {
+//             e.target.parentElement.parentElement.parentElement.parentElement.classList.toggle("OpenSlide")
+//           })
+//      })
+// }
 const breadCrum_Date = document.querySelector('.breadCrum_Date ');
 breadCrum_Date.innerHTML = new Date().toDateString();
 
